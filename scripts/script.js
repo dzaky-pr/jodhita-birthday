0b5dabfdcbc01d82c39fcb787345ca015ec366b4
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-04-01 08:41:15 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 function onClickOdhi() {
     alert('Ooopss, apaan nihh???');
-    window.location.href='https://www.youtube.com/watch?v=oIYWenB637c';
+    window.location.href='https://open.spotify.com/track/3pCt2wRdBDa2kCisIdHWgF?si=2a4c4f1cfd994ab8';
 }

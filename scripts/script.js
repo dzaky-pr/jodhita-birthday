27f6dc84393d4bfe2b0d73e04298f5dd8f082bba
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-05-14 15:58:49 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,7 @@ function onClickOdhi() {
     alert('Ooopss, apaan nihh???');
     window.location.href='https://www.youtube.com/watch?v=oIYWenB637c';
 }
+
+function info() {
+    alert('Please open with PC/Laptop :D');
+}
